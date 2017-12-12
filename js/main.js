338f4719da443500407bf855e340ad9835310163
Teresa Marban <teresamarbancastro@gmail.com>
2017-12-12 10:08:07 +0100

    Bicheando con colores

--- js/main.js
+++ js/main.js
@@ -1,9 +1,35 @@
 'use strict';
-var negative = document.querySelector('.punishment').innerHTML;
+var negative = document.querySelector('.punishment').outerHTML;
+var choose = document.querySelector('.colour').outerHTML;
+var x = "";
 function repeat(){
 	for (var i = 0; i <= 100; i++){
-	negative += '<br> He aprendido bien cómo funcionan los bucles' + '<br>';
+	x += negative;
 	}
-return negative;
+return x;
 }
 document.write(repeat());
+/*
+document.addEventListener('DOMContentLoaded', function(){
+	document.querySelector('select[name="select"]').onchange=changeEventHandler;
+},false);
+
+function changeEventHandler(event){
+	if(!event.target.value){
+		negative.classList.add('.red');
+
+	}
+}
+*/
+var paint = document.querySelector('.colour');
+paint.addEventListener('click', comparar);
+function comparar(){
+
+	if (paint === '#red') {
+		document.querySelector('.colour').innerHTML
+	}else if (paint === '#blue'){
+		document.querySelector('.colour').innerHTML
+
+	}else (paint === '#blue'){
+		document.querySelector('.colour').innerHTML
+	}
