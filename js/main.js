8818dca2e1b23d80528ae9b68819f73428bc778a
Teresa Marban <teresamarbancastro@gmail.com>
2017-12-12 09:14:27 +0100

    100 repeats done

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 'use strict';
+var negative = document.querySelector('.punishment').innerHTML;
+function repeat(){
+	for (var i = 0; i <= 100; i++){
+	negative += '<br> He aprendido bien cómo funcionan los bucles' + '<br>';
+	}
+return negative;
+}
+document.write(repeat());
